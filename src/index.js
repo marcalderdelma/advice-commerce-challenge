9f45d68a328f84242b13d28f6e783180ce272f8f
Marc-Alder Delma <marcalderdema.mad@gmail.com>
2019-03-10 20:58:16 -0300

    :)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // elemento de dropdown
+    // Dropdown element
     var dropdown = document.querySelectorAll('.dropdown-trigger');
+    // Mobile Menu element (Sidenav)
     var sidenav = document.querySelectorAll('.sidenav');
+
+    // Dropdown event
     var dropdownInstance = M.Dropdown.init(dropdown);
+    // Sidenav event
     var sidenavInstance = M.Sidenav.init(sidenav);
   });
