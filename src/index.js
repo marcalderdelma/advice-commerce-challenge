af368794e02c851a9d6bfc9902dad9cabf5e4bd2
Marc-Alder Delma <marcalderdema.mad@gmail.com>
2019-03-10 20:54:33 -0300

    Fase final do projeto: pronto para entraga. :)

--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, open);
+    // elemento de dropdown
+    var dropdown = document.querySelectorAll('.dropdown-trigger');
+    var sidenav = document.querySelectorAll('.sidenav');
+    var dropdownInstance = M.Dropdown.init(dropdown);
+    var sidenavInstance = M.Sidenav.init(sidenav);
   });
-
-    // Initialize collapsible (uncomment the lines below if you use the dropdown variation)
-  var collapsibleElem = document.querySelector('.collapsible');
-  var collapsibleInstance = M.Collapsible.init(collapsibleElem, open);
-
-
-$( document ).ready( function () {
-   $('.dropdown-trigger').dropdown({
-       hoover: false
-   });
-});
